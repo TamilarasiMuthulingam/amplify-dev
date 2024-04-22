92cb45263e61e08112dd73cd54d79b5ad4f24b28
saikiran9087 <saikiran70625@gmail.com>
2024-04-22 12:05:50 +0530

    test10

--- amplify-backup/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "amplifydev": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
