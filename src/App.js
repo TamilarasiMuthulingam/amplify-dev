e305266ec39a897916d9913ced638d5419551fec
saikiran9087 <130025811+saikiran9087@users.noreply.github.com>
2024-05-27 12:35:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BuildingData from "./components/Buildings/BuildingData";
+import SelectedItem from "./components/Buildings/SelectedItem";
+import EquipmentData from "./components/Buildings/EquipmentData"
 import Loginpage from "./LoginPage";
+
 import LeftNavigation from "./components/Navigation/LeftNavigation";
 import Home from "./components/Buildings/Home";
 import AllFamilyEquipments from "./components/Buildings/AllFamilyEquipments";
 import EquipmentList from "./components/Buildings/EquipmentList";
 import EquipmentSummary from "./components/Buildings/EquipmentSummary";
-import LineChart from "./Chart";
 import { Authenticator } from '@aws-amplify/ui-react';
 
 function App() {
 
+    return (
   const DefaultContainer = () => (
     <>
       <LeftNavigation />
@@ -18,8 +22,6 @@ function App() {
       <Route exact path="/AllFamilyEquipments" component={AllFamilyEquipments} />
       <Route exact path="/EquipmentList" component={EquipmentList} />
       <Route exact path="/EquipmentSummary" component={EquipmentSummary} />
-      <Route exact path="/ChartData" component={LineChart} />
-
     </>
   )
 
@@ -27,7 +29,7 @@ function App() {
     <Authenticator.Provider>
       <Router>
         <Switch>
-        <Route exact path="/" component={Loginpage} />
+          <Route exact path="/" component={Loginpage} />
           <Route exact path="/LoginPage" component={Loginpage} />
           <Route component={DefaultContainer}/>
         </Switch>
@@ -35,5 +37,22 @@ function App() {
     </Authenticator.Provider>
   );
 }
+
+        <Router>
+
+
+          <Switch>
+          <Route exact path ="/" component={Loginpage}/>
+            <Route exact path="/BuildingData" component={BuildingData} />
+          <Route exact path="/SelectedItem" component={SelectedItem}/>
+          <Route exact path="/EquipmentData" component={EquipmentData}/>
+
+          </Switch>
+
+        </Router>
+
+    );
+  }
+
 
 export default App;
