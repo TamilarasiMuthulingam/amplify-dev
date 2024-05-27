bf8569cffe6175e3b04fe8c84e238401edbb2eb8
saikiran9087 <130025811+saikiran9087@users.noreply.github.com>
2024-05-27 10:40:22 +0530

    Update EquipmentSummary.js

--- src/components/Buildings/EquipmentSummary.js
+++ src/components/Buildings/EquipmentSummary.js
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { generateClient } from "aws-amplify/api";
-import { getAllRecords2, getAllRecords3 } from "../../graphql/queries";
-
+import { getAllEquipmentSummary, latestTimeStamp } from "../../graphql/queries";
+import { useHistory } from 'react-router-dom';
+import EnumData from "./EnumFile";
+ 
 const client = generateClient();
-
+ 
 function EquipmentSummary(BuildingData) {
     console.log('EquipmentName from summary', BuildingData?.location?.EquipmentName);
     let gSelectedEquipment = BuildingData?.location?.EquipmentName;
     const [Equipment, setEquipment] = useState([]);
     const [EquipmentProperties, setEquipmentProperties] = useState([]);
     const [Points, setPoints] = useState([]);
-
+    const history = useHistory();
+ 
     useEffect(() => {
         const fetchData = async () => {
-            if(gSelectedEquipment !== undefined){
+            if (gSelectedEquipment !== undefined) {
                 try {
-                    const result1 = await client.graphql({ query: getAllRecords2 });
-                    const result2 = await client.graphql({ query: getAllRecords3 });
-                    let lSelectedEquipment = result1?.data?.getAllRecords2?.filter((obj) => fetchSiteFamilyEquip(obj.ObjectMap) === gSelectedEquipment);
-                    let lSelectedEquipmentPoints = result2?.data?.getAllRecords3?.filter((obj) => fetchSiteFamilyEquip(obj.PointMap) === gSelectedEquipment);
+                    let serialNumber = gSelectedEquipment;
+                    const result1 = await client.graphql({ query:getAllEquipmentSummary,variables: { serialNumber } });
+                    const result2 = await client.graphql({ query: latestTimeStamp, variables: { serialNumber } });
+ 
+                    let lSelectedEquipment = result1.data.getAllEquipmentSummary.items;
+                    console.log("hii")
+
+                    let lSelectedEquipmentPoints = result2.data.getAllRecords4.LatestTimestamp.items;
+ 
                     let lFormattedProperties = lSelectedEquipment[0]?.Points && JSON.parse(lSelectedEquipment[0]?.Points?.replace(/'/g, '"'));
                     setEquipment(lSelectedEquipment[0]);
                     setEquipmentProperties(lFormattedProperties);
@@ -28,45 +36,66 @@ function EquipmentSummary(BuildingData) {
                 }
             }
         };
-
         fetchData();
     }, [gSelectedEquipment]);
-
+ 
     const fetchSiteFamilyEquip = (Name) => {
-        let lName = Name.split('#')
+        let lName = Name.split('#');
         return lName[0] + "#" + lName[1] + "#" + lName[2];
-    }
-
+    };
+ 
     const GetPointValueForProperty = (property) => {
         let lObject = fetchSiteFamilyEquip(Equipment?.ObjectMap) + "#" + property;
         let Propertylst = Points?.filter((ele) => ele.PointMap === lObject);
+ 
         if (Propertylst?.length > 0) {
-            Propertylst.sort((a, b) => new Date(b.Timestamp) - new Date(a.Timestamp));
-            return Propertylst[0]?.Value;
+            let value = Propertylst[0]?.Value;
+            if (value !== undefined) {
+                if (property.includes("heatCoolMode")) {
+                   
+                    return EnumData.HeatCoolMode("Mode." + value + ".Text");
+                    
+                } else if (property.includes("occupancyStatus")) {
+                    return EnumData.PresentValue("PresentValue." + value + ".Text");
+                }
+                else if(property.includes("HeatCoolModeStatus")){
+                    return EnumData.HeatCoolMode("Mode." + value + ".Text");
+                }
+                // console.log("value",EnumData.PresentValue("PresentValue." + value + ".Text"))
+                // console.log("hi",EnumData.HeatCoolMode("Mode." + value + ".Text"))
+                return parseFloat(value).toFixed(2);
+            }
         }
-    }
-
+        return "---";
+    };
+ 
+    const RedirectToChart = (keyname) => {
+        history.push({ pathname: "/ChartData", keyname: keyname });
+    };
+ 
     return (
         <>
-            <div className="content-pane"><h4>Equipment Summary</h4></div>
-            <table className="tblequipment" style={{"width":"50%", "margin-left":"310px"}} cellSpacing="0" cellPadding="0">
+            <div className="content-pane">
+                <h4>Equipment Summary</h4>
+            </div>
+            <table className="tblequipment" style={{ width: "50%", marginLeft: "310px" }} cellSpacing="0" cellPadding="0">
                 <thead>
-                    <th>Keyname</th>
-                    <th>Current Value</th>
+                    <tr>
+                        <th>Keyname</th>
+                        <th>Current Value</th>
+                    </tr>
                 </thead>
                 <tbody className="cell_container">
-                    {
-                        EquipmentProperties?.map((obj, index) => (
-                            <tr key={index}>
-                                <td>{obj.keyname}</td>
-                                <td>{GetPointValueForProperty(obj.keyname)}</td>
-                            </tr>
-
-                        ))
-                    }
+                    {EquipmentProperties?.map((obj, index) => (
+                        <tr key={index}>
+                            <td style={{ cursor: "pointer" }} onClick={() => RedirectToChart(obj.keyname)}>{obj.keyname}</td>
+                            <td>{GetPointValueForProperty(obj.keyname)}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </>
-    )
+    );
 }
+ 
 export default EquipmentSummary;
